15129b5dc38fc8a352bdfae8dc5e1b25403b26c1
Harun Wafula <harun@Davids-MacBook-Air.local>
2022-12-16 13:47:17 +0200

    added theme support with three different themes

--- public/settings.js
+++ public/settings.js
@@ -12,6 +12,9 @@ var closeSettingsBtn = document.getElementById("close-settings-btn");
 var settingsMenu = document.getElementById("settings-menu");
 var completionStatus = document.getElementById("completion-status");
 
+var siteColors = document.querySelectorAll(".site-color");
+var selectedTheme = document.getElementsByClassName("site-color active")[0];
+
 var showDuplicates = true;
 var showHighlights = true;
 
@@ -44,8 +47,20 @@ function onInit () {
             completionStatus.style.display = "none";
 
         }
-    })
+    });
+
+    siteColors.forEach((picker)=>{
+        picker.addEventListener("click", ()=>{
+
+            selectedTheme.classList.remove("active");
+            picker.classList.add("active");
+            selectedTheme = picker;
+            setTheme();
+        });
+    });
 }
+
+
 
 function openSettings() {
     settingsMenu.style.display = "block";
@@ -60,6 +75,30 @@ function toggleDuplicates() {
 function toggleHighlights() {
     showHighlights = !showHighlights;
     console.log("Show highlights : " + showHighlights);
+}
+
+function setTheme() {
+    switch(selectedTheme.attributes["name"].value){
+        case "theme-dark": 
+            document.body.style.backgroundColor = "rgb(30, 30, 30)";
+            document.body.style.color = "white";
+            settingsMenu.getElementsByClassName("main-body")[0].style.backgroundColor = "rgb(30, 30, 30)";
+            break;
+        case "theme-baby-blue":
+            document.body.style.backgroundColor = "#367eb8";
+            document.body.style.color = "black";
+            settingsMenu.getElementsByClassName("main-body")[0].style.backgroundColor = "white";
+
+            break;
+        case "theme-baige":
+            document.body.style.backgroundColor = "rgb(255, 222, 203)";
+            document.body.style.color = "black";
+            settingsMenu.getElementsByClassName("main-body")[0].style.backgroundColor = "white";
+
+            break;
+        
+    }
+    
 }
 
 
